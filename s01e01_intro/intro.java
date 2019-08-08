544dd827f18aeafaab02ca45940b64087bfe34d2
David Sena <sena.ufc@gmail.com>
2019-08-08 12:07:56 -0300

    up

---
+++ s01e01_intro/intro.java
@@ -0,0 +1,19 @@
+public class MyClass {
+    public static void main(String args[]) {
+      int a = 3;
+      double b = 3.4;
+      String d = "banana";
+      int[] v = {3, 2, 1, 4};
+      
+      for(int i = 0; i < v.length; i++){
+          System.out.println(v[i]);
+      }
+      
+      ArrayList<int> v2 ... //collections
+      
+      for(int elem : v2)
+        System.out.println(v2);
+      
+      System.out.println("" + a + " " + b + " " + d);
+    }
+}
---
+++ s01e01_intro/intro.py
@@ -0,0 +1,9 @@
+b = 3
+c = 3.4
+d = "banana"
+print (b, c, d)
+
+v = [3, 2.3, "1", 4]
+
+for elem in v:
+    print(elem)
