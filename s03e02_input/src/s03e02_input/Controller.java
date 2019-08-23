8a545fd2bf089dba20f8a570905f87d0bf5be31b
David Sena <sena.ufc@gmail.com>
2019-08-23 12:06:02 -0300

    up

--- s03e02_input/src/s03e02_input/Controller.java
+++ s03e02_input/src/s03e02_input/Controller.java
@@ -5,16 +5,34 @@ import java.util.Scanner;
 class Peixe{
 	int barriga = 0;
 	int maxBarriga = 5;
+	boolean isAlive = true;
 	void alimentar(int qtd){ //aumenta a quantidade de comida na barriga até o máximo
-		barriga += qtd;
-		System.out.println("Nhami nhami");
+		if(isAlive) {
+			barriga += qtd;
+			System.out.println("Nhami nhami");
+			if(barriga > maxBarriga) {
+				System.out.println("explodiu");
+				isAlive = false;
+			}
+				
+		}
 	}
 	void esperar() { //diminui a quantidade de comida, 1 por vez, ate zero
-		barriga -= 1;
-		System.out.println("Affffffffff");
+		if(isAlive) {
+			barriga -= 1;
+			System.out.println("Affffffffff");
+			if(barriga < 0) {
+				System.out.println("Definou");
+				isAlive = false;
+			}
+		}
 	}
 	void status() {
-		System.out.println("Vivo com a barriga: " + barriga);
+		if(isAlive)
+			System.out.print("Vivo");
+		else
+			System.out.print("Morto");
+		System.out.println(" com a barriga: " + barriga);
 	}
 }
 public class Controller {
@@ -27,15 +45,16 @@ public class Controller {
 			String line = scanner.nextLine();
 			String[] vet = line.split(" ");
 			
-			if(vet[0].equals("feed")) {
+			if(vet[0].equals("feed"))
 				marlin.alimentar(Integer.parseInt(vet[1]));
-			}
 			else if(vet[0].equals("wait"))
 				marlin.esperar();
 			else if(vet[0].equals("show"))
 				marlin.status();
 			else if(vet[0].equals("end"))
 				break;
+			else
+				System.out.println("fail: comando invalido");
 		}
 	}
 }
